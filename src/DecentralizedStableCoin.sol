e1815be6c1e9fe816a1f64b4ac589903c57e270c
IftequarIffu <iftequar.154@gmail.com>
2024-10-21 11:26:41 +0530

    Added DecentalizedStableCoin contract with NatSpec comments

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @author  Iftequar Ahmed
+ * @title   DecntralizedStableCoin contract
+ * @dev     Iftequar Ahmed
+ * @notice  This is a contract meant to be governed by DSCEngine.
+ This is just an ERC20 implementation of the stable coin.
+ */
+
+contract DecntralizedStableCoin {
+    
+}
